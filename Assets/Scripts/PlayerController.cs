bbc319ecf84910f4718477d7f5e1282d1426456d
user <user@SUZAKU>
2016-05-29 02:13:32 -0500

    mover file creation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public Boundary bd;
 
     private Rigidbody rb;
 
@@ -14,5 +24,16 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, bd.xMin, bd.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, bd.zMin, bd.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
 }
