f6fd4d6ac479dfa3fcd3d1daf74ae4cda36602b6
user <user@SUZAKU>
2016-06-16 18:00:28 -0500

    bg scroller added

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+
+    private float tileSizeZ;
+    private Vector3 startPosition;
+    private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+
+        myTransform = GetComponent<Transform>();
+        startPosition = myTransform.position;
+        tileSizeZ = myTransform.localScale.y;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        myTransform.position = startPosition + Vector3.forward * newPosition;
+	
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -40,6 +40,7 @@ public class GameController : MonoBehaviour {
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Instantiate(hazard, spawnPosition, Quaternion.identity);
                 yield return new WaitForSeconds(spawnWait);
