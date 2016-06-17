affee49a8e9607ed31c2be9ac2ef834e23115558
user <user@SUZAKU>
2016-06-16 19:46:22 -0500

    weapon controller

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+
+    private AudioSource aSrc;
+
+	// Use this for initialization
+	void Start () {
+
+        aSrc = GetComponent<AudioSource>();
+        InvokeRepeating("Fire", delay, fireRate);
+	}
+	
+    void Fire ()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        aSrc.Play();
+    }
+	
+}
