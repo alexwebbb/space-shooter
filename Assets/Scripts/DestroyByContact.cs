132d90506d594ae7637b4d92b15404f586aec9b4
user <user@SUZAKU>
2016-06-18 08:10:44 -0500

    enemy ship dodging implemented

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -23,13 +23,22 @@ public class DestroyByContact : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Boundary") { return; }
-        Instantiate(explosion, transform.position, transform.rotation);
-        if (other.tag == "Player")
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+        
+        if (other.CompareTag("Player"))
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
             gc.GameOver();
         }
+
         gc.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
